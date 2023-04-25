6b4087074cea618cd0eeecdaa29dc262b1bd2f1d
Juan Pablo Sanchez <Juan Pablo Sanchez@DESKTOP-U4HB8MQ>
2023-04-24 21:04:29 -0500

    Mejoramos codigo, agregamos nuevas funcionalidades y ahora falta hacer el metodo para cambiar informacion de las actividades y la insecion de subactividades

--- api.js
+++ api.js
@@ -1,99 +1,117 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const db = require('./db');
-const cors = require('cors')
-const morgan = require('morgan')
-const express = require('express');
+const db = require("./db");
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
 
 const app = express();
 
-app.use(cors())
-app.use(morgan('combined'))
+app.use(cors());
+app.use(morgan("combined"));
 
-const port = process.env.PORT || 3000   
-
+const port = process.env.PORT || 3000;
 
 // Authentication
-app.get('/users/:user', async(req, res) =>{
-    const {username,password} = JSON.parse(req.params.user)
-    const data = await db.getUser(username,password)
-    res.send(data)
-})
+app.get("/users/:user", async (req, res) => {
+  const { username, password } = JSON.parse(req.params.user);
+  const data = await db.getUser(username, password);
+  res.send(data);
+});
 
-app.get('/users', async(req, res) => {
-    const users = await db.getUsers()
-    console.log(users)
-    res.send(users)
-})
+app.get("/users", async (req, res) => {
+  const users = await db.getUsers();
+  console.log(users);
+  res.send(users);
+});
 
-app.put('/users/:user', async(req, res) => {
-    const {id_user,username,name,surname,email,id_role,id_area} = JSON.parse(req.params.user)
-    const query = await db.updateUser(id_user,username,name,surname,email,id_role,id_area)
-    console.log(query)
-    res.send(query)
-})
+app.put("/users/:user", async (req, res) => {
+  const { id_user, username, name, surname, email, id_role, id_area } =
+    JSON.parse(req.params.user);
+  const query = await db.updateUser(
+    id_user,
+    username,
+    name,
+    surname,
+    email,
+    id_role,
+    id_area
+  );
+  console.log(query);
+  res.send(query);
+});
 
-app.get('/colab/:area',async(req, res) => {
-    const area = req.params.area
-    const users = await db.getColab(area)
-    console.log(users)
-    res.send(users)
+app.get("/colab/:area", async (req, res) => {
+  const area = req.params.area;
+  const users = await db.getColab(area);
+  console.log(users);
+  res.send(users);
+});
 
-})
+app.post("/activity/:data", async (req, res) => {
+  const { title, mandated, description, relevance, date_start, date_end } =
+    JSON.parse(req.params.data);
+  const activity = await db.createActivity(
+    title,
+    mandated,
+    description,
+    relevance,
+    date_start,
+    date_end
+  );
+  res.send(activity);
+});
 
-app.post('/activity/:data',async(req, res) => {
-    const {title,mandated,description,relevance,date_start,date_end} = JSON.parse(req.params.data)
-    const activity = await db.createActivity(title,mandated,description,relevance,date_start,date_end)
-    res.send(activity)
-})
-
-app.get('/activity',async(req,res)=>{
-    const activities = await db.getActivities()
-    console.log(activities)
-    res.send(activities)
-
-})
-
+app.get("/activity", async (req, res) => {
+  const activities = await db.getActivities();
+  console.log(activities);
+  res.send(activities);
+});
 
 // Change password
-app.put('/users/role/:user', async(req, res) =>{
-    const {username,new_password} = JSON.parse(req.params.user)
-    const data = await db.change_password(username,new_password)
-    console.log(data)
-    res.send(data)
-})
-
+app.put("/users/role/:user", async (req, res) => {
+  const { username, new_password } = JSON.parse(req.params.user);
+  const data = await db.change_password(username, new_password);
+  console.log(data);
+  res.send(data);
+});
 
 // Change role
-app.put('/users/password/:user', async(req, res) =>{
-    const {username,new_role} = JSON.parse(req.params.user)
-    const data = await db.change_rol(username,new_role)
-    console.log(data)
-    res.send(data)
-})
+app.put("/users/password/:user", async (req, res) => {
+  const { username, new_role } = JSON.parse(req.params.user);
+  const data = await db.change_rol(username, new_role);
+  console.log(data);
+  res.send(data);
+});
 
 //Create a new user
-app.post('/users/:user', async(req, res) =>{
-    const {name,surname,email,role,area} = JSON.parse(req.params.user)
-    const username = (name[0] + surname).toLowerCase() + new Date().getFullYear()
-    const data = await db.createUser(username,name.toLowerCase(),surname.toLowerCase(),email.toLowerCase(),role,area)
-    res.send(data)
-})
+app.post("/users/:user", async (req, res) => {
+  const { name, surname, email, role, area } = JSON.parse(req.params.user);
+  const username = (name[0] + surname).toLowerCase() + new Date().getFullYear();
+  const data = await db.createUser(
+    username,
+    name.toLowerCase(),
+    surname.toLowerCase(),
+    email.toLowerCase(),
+    role,
+    area
+  );
+  res.send(data);
+});
 
-app.put('/password/:data',async (req,res) => {
-    const {username,new_password} = JSON.parse(req.params.data)
-    await db.changePassword(username,new_password)
-    const user = await db.getUser(username,new_password)
-    res.send(user)
-})
+app.put("/password/:data", async (req, res) => {
+  const { username, new_password } = JSON.parse(req.params.data);
+  await db.changePassword(username, new_password);
+  const user = await db.getUser(username, new_password);
+  res.send(user);
+});
 
-app.get('/areas',async (req,res) =>{
-    const areas = await db.getAreas()
-    console.log(areas)
-    res.send(areas)
-})
+app.get("/areas", async (req, res) => {
+  const areas = await db.getAreas();
+  console.log(areas);
+  res.send(areas);
+});
 
-
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
--- db.js
+++ db.js
@@ -1,95 +1,132 @@
-require('dotenv').config();
-const {Pool} = require('pg');
-const md5 = require('md5');
+require("dotenv").config();
+const { Pool } = require("pg");
+const md5 = require("md5");
 
 const pool = new Pool({
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    host: process.env.DBHOST,
-    port: process.env.DBPORT,
-    database: process.env.DATABASE
-})
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT,
+  database: process.env.DATABASE,
+});
 
-const getUser = async(username,password) =>{
-    const user = await pool.query(`SELECT  us.id_user, us.name,us.surname, us.email, us.username, ro.role_description,ar.area_description,us.id_area FROM users us, roles ro, areas ar WHERE us.username = '${username}' AND us.password='${md5(password)}' AND us.id_area = ar.id_area AND us.id_role = ro.id_role`)
-    // const user = await pool.query(`SELECT us.id_user,us.username,us.password,us.name,us.surname,us.email,us.birthdate,ro.role_name FROM users us, roles ro WHERE us.username='${username}' AND us.password='${md5(password)}' AND us.role = ro.id_role `)
-    return user.rows[0]
-}
+const getUser = async (username, password) => {
+  const user = await pool.query(
+    `SELECT  us.id_user, us.name,us.surname, us.email, us.username, ro.role_description,ar.area_description,us.id_area FROM users us, roles ro, areas ar WHERE us.username = '${username}' AND us.password='${md5(
+      password
+    )}' AND us.id_area = ar.id_area AND us.id_role = ro.id_role`
+  );
+  // const user = await pool.query(`SELECT us.id_user,us.username,us.password,us.name,us.surname,us.email,us.birthdate,ro.role_name FROM users us, roles ro WHERE us.username='${username}' AND us.password='${md5(password)}' AND us.role = ro.id_role `)
+  return user.rows[0];
+};
 
-const changePassword = async(username,new_password) =>{
-    const res = await pool.query(`UPDATE users SET password='${md5(new_password)}' WHERE username='${username}'`)
-    return res
-}
+const changePassword = async (username, new_password) => {
+  const res = await pool.query(
+    `UPDATE users SET password='${md5(
+      new_password
+    )}' WHERE username='${username}'`
+  );
+  return res;
+};
 
-const change_rol = async(username,new_role) =>{
-    const res = await pool.query(`UPDATE users SET role='${new_role}' WHERE username='${username}'`)
-    return res.rowCount
-}
+const change_rol = async (username, new_role) => {
+  const res = await pool.query(
+    `UPDATE users SET role='${new_role}' WHERE username='${username}'`
+  );
+  return res.rowCount;
+};
 
-const getUsers = async() =>{
-    const res = await pool.query(`SELECT  us.id_user,us.name,us.surname, us.email, us.username, ro.role_description,ar.area_description,us.id_role,us.id_area FROM users us, roles ro, areas ar WHERE us.id_area = ar.id_area AND us.id_role = ro.id_role ORDER BY us.name`)
-    console.log(res.rows)
-    return res.rows
-}
+const getUsers = async () => {
+  const res = await pool.query(
+    `SELECT  us.id_user,us.name,us.surname, us.email, us.username, ro.role_description,ar.area_description,us.id_role,us.id_area FROM users us, roles ro, areas ar WHERE us.id_area = ar.id_area AND us.id_role = ro.id_role ORDER BY us.name`
+  );
+  console.log(res.rows);
+  return res.rows;
+};
 
-const getColab = async(id_area) => {
-    const res = await pool.query(`SELECT id_user,INITCAP(CONCAT (name, ' ', surname)) AS full_name FROM users WHERE id_role = 3 AND id_area = ${id_area} ORDER BY full_name`)
-    return res.rows
-}
+const getColab = async (id_area) => {
+  const res = await pool.query(
+    `SELECT id_user,INITCAP(CONCAT (name, ' ', surname)) AS full_name FROM users WHERE id_role = 3 AND id_area = ${id_area} ORDER BY full_name`
+  );
+  return res.rows;
+};
 
-const get_users_by = async({id,username,name,surname,email,role}) =>{
-    const res = await pool.query(`SELECT id_user,username,name,surname,email,role FROM users WHERE username ORDER BY name`)
-    return res.rows
-}
+const get_users_by = async ({ id, username, name, surname, email, role }) => {
+  const res = await pool.query(
+    `SELECT id_user,username,name,surname,email,role FROM users WHERE username ORDER BY name`
+  );
+  return res.rows;
+};
 
-const createActivity = async(title,mandated,description,relevance,date_start,date_end) => {
-    try{
-        const res = await pool.query(`INSERT INTO activities (activity_title,activity_description,activity_mandated,relevance,date_start,date_end) VALUES ('${title}','${description}',${mandated},${relevance},'${date_start}','${date_end}')`)
-        return res
-    }catch(e){
-        return e
-    }
+const createActivity = async (
+  title,
+  mandated,
+  description,
+  relevance,
+  date_start,
+  date_end
+) => {
+  try {
+    const res = await pool.query(
+      `INSERT INTO activities (activity_title,activity_description,activity_mandated,relevance,date_start,date_end) VALUES ('${title}','${description}',${mandated},${relevance},'${date_start}','${date_end}')`
+    );
+    return res;
+  } catch (e) {
+    return e;
+  }
+};
 
-}
+const createUser = async (username, name, surname, email, role, area) => {
+  const query = await pool.query(
+    `INSERT INTO users (username,password,name,surname,email,id_role,id_area) VALUES ('${username}','${md5(
+      username
+    )}','${name}','${surname}','${email}','${role}','${area}')`
+  );
+  return query;
+};
 
-const createUser = async(username,name,surname,email,role,area)=>{
-    const query = await pool.query(`INSERT INTO users (username,password,name,surname,email,id_role,id_area) VALUES ('${username}','${md5(username)}','${name}','${surname}','${email}','${role}','${area}')`)
-    return query
-}
+const getActivities = async () => {
+  const activities = await pool.query(
+    `SELECT ac.id_activity,ac.activity_title,ac.activity_description,INITCAP(CONCAT (us.name, ' ', us.surname)) AS full_name,ac.relevance, to_char(ac.date_start,'YYYY/MM/DD') as date_start,to_char(ac.date_end,'YYYY/MM/DD') as date_end,st.state_description, ac.activity_mandated, st.id_state from activities ac, users us, states st where ac.activity_mandated = us.id_user AND ac.id_state = st.id_state ORDER BY ac.activity_title`
+  );
+  return activities.rows;
+};
 
+const get_activities_by = async (state) => {
+  const activities = await pool.query(`SELECT * FROM activities`);
+};
 
-const getActivities = async() =>{
-    const activities = await pool.query(`SELECT ac.id_activity,ac.activity_title,ac.activity_description,INITCAP(CONCAT (us.name, ' ', us.surname)) AS full_name,ac.relevance, to_char(ac.date_start,'YYYY/MM/DD') as date_start,to_char(ac.date_end,'YYYY/MM/DD') as date_end,st.state_description from activities ac, users us, states st where ac.activity_mandated = us.id_user AND ac.id_state = st.id_state ORDER BY ac.activity_title`)
-    return activities.rows
-}
+const getAreas = async () => {
+  const areas = await pool.query("SELECT * FROM areas");
+  return areas.rows;
+};
 
-const get_activities_by = async(state) =>{
-    const activities = await pool.query(`SELECT * FROM activities`)
-}
-
-const getAreas = async() => {
-    const areas = await pool.query('SELECT * FROM areas')
-    return areas.rows
-
-}
-
-
-const updateUser = async(id_user,username,name,surname,email,id_role,id_area) => {
-    const query = await pool.query(`UPDATE users SET name ='${name}', surname ='${surname}', email ='${email}', id_role =${id_role} , id_area=${id_area}, username='${username}' WHERE id_user = ${id_user}`)
-    return query
-}
+const updateUser = async (
+  id_user,
+  username,
+  name,
+  surname,
+  email,
+  id_role,
+  id_area
+) => {
+  const query = await pool.query(
+    `UPDATE users SET name ='${name}', surname ='${surname}', email ='${email}', id_role =${id_role} , id_area=${id_area}, username='${username}' WHERE id_user = ${id_user}`
+  );
+  return query;
+};
 
 module.exports = {
-    getUser,
-    getUsers,
-    changePassword,
-    change_rol,
-    createActivity,
-    createUser,
-    getActivities,
-    get_activities_by,
-    get_users_by,
-    getAreas,
-    getColab,
-    updateUser
-}
+  getUser,
+  getUsers,
+  changePassword,
+  change_rol,
+  createActivity,
+  createUser,
+  getActivities,
+  get_activities_by,
+  get_users_by,
+  getAreas,
+  getColab,
+  updateUser,
+};
